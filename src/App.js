a2467056c15ae450c0085a4439dfca11581fdefa
John Furlong <johnfurlong24@gmail.com>
2021-01-17 16:29:59 -0700

    Adding a link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <a href="https://www.github.com/jfur1">My Code</a>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

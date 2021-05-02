59533a7f494640376cbed431a2d686c0397fdf4b
John Furlong <johnfurlong24@gmail.com>
2021-05-01 19:00:46 -0600

    front end work on personal site

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,5 +1,7 @@
 import Nav  from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { useState } from 'react';
+import Fade from 'react-reveal/Fade';
 
 export const NavBar = props => {
     console.log(props)
@@ -11,19 +13,22 @@ export const NavBar = props => {
     }
 
     return (
-        <Nav justify variant="tabs my-3" activeKey={key} onSelect={selectKey}>
-            <Nav.Item>
-                <Nav.Link eventKey="home" href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="education" href="/education">Education</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="projects" href="/projects">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="contact" href="/contact">Contact</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <Fade top duration={1600}>
+            <Nav justify variant="tabs my-3" activeKey={key} onSelect={selectKey}>
+            <Navbar.Brand className="ml-5" href="/home" >John Furlong</Navbar.Brand>
+                <Nav.Item>
+                    <Nav.Link eventKey="home" href="/home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="education" href="/education">Education</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="projects" href="/projects">Projects</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="contact" href="/contact">Contact</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Fade>
     )
 }

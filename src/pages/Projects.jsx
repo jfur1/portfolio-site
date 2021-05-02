49ab29e5bd9e0cdd00fa3bb2887837498f6f9099
John Furlong <johnfurlong24@gmail.com>
2021-05-02 08:26:13 -0600

    Update projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -24,37 +24,37 @@ export function Projects(props) {
                 <div className="projects">
                     <Project 
                         project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
+                        description="React application to visualize various pathfinding algorithms."    
                         created="January 2021"
                     />
                     <Project 
-                        project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
+                        project = "my-portfol.io"
+                        description="Full stack web application for users to host their portfolios on."    
                         created="January 2021"
                     />
                     <Project 
-                        project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
+                        project = "Forte Music App"
+                        description="Full stack application to match musicians with local tutors."    
+                        created="May 2019"
+                    />
+                    <Project 
+                        project = "DNS Name Resolution Engine"
+                        description="Multi-threaded application, written in C, which resolves domain names to IP addresses."    
+                        created="July 2020"
+                    />
+                    <Project 
+                        project = "Blob Detection"
+                        description="Python program to detect blobs from sample images."    
                         created="January 2021"
                     />
                     <Project 
-                        project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
-                        created="January 2021"
-                    />
-                    <Project 
-                        project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
-                        created="January 2021"
-                    />
-                    <Project 
-                        project = "Pathfinding Visualizer"
-                        description="Website for users to host their portfolios on!"    
+                        project = "Sorting Visualizer"
+                        description="React application to visualize various sorting algorithms."    
                         created="January 2021"
                     />
                 </div>
                 <div className="tab-text mt-5" style={{textAlign: "center"}}>
-                    <a href="https://github.com/jfur1" target="_blank" rel="noreferrer" style={{display: "inline-block"}} >
+                    <a href="https://github.com/jfur1?tab=repositories" target="_blank" rel="noreferrer" style={{display: "inline-block"}} >
                         <div className="btn btn-outline-primary">More Projects</div>
                     </a>
                 </div>
